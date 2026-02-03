df5196ad9f6a0944ded4333122097a8aeff517d3
Sebastian Peters <git@nebu2k.de>
2026-02-03 18:29:08 +0100

    feat(terraform): Use local variables for Pangolin DNS subdomains in Cloudflare records

--- terraform/hetzner/dns.tf
+++ terraform/hetzner/dns.tf
@@ -2,10 +2,16 @@
 # Cloudflare DNS Records
 # ===========================================
 
+# Extract subdomain from FQDN (e.g., "pangolin.elmstreet79.de" -> "pangolin")
+locals {
+  pangolin_subdomain     = split(".", var.pangolin_domain)[0]
+  pangolin_api_subdomain = split(".", var.pangolin_api_domain)[0]
+}
+
 # Main Pangolin domain
 resource "cloudflare_record" "pangolin" {
   zone_id = var.cloudflare_zone_id
-  name    = "pangolin"
+  name    = local.pangolin_subdomain
   content = hcloud_server.pangolin.ipv4_address
   type    = "A"
   ttl     = 300
@@ -15,7 +21,7 @@ resource "cloudflare_record" "pangolin" {
 # Pangolin API domain (Integration API)
 resource "cloudflare_record" "pangolin_api" {
   zone_id = var.cloudflare_zone_id
-  name    = "pangolin-api"
+  name    = local.pangolin_api_subdomain
   content = hcloud_server.pangolin.ipv4_address
   type    = "A"
   ttl     = 300
@@ -25,7 +31,7 @@ resource "cloudflare_record" "pangolin_api" {
 # IPv6 records
 resource "cloudflare_record" "pangolin_ipv6" {
   zone_id = var.cloudflare_zone_id
-  name    = "pangolin"
+  name    = local.pangolin_subdomain
   content = hcloud_server.pangolin.ipv6_address
   type    = "AAAA"
   ttl     = 300
@@ -34,7 +40,7 @@ resource "cloudflare_record" "pangolin_ipv6" {
 
 resource "cloudflare_record" "pangolin_api_ipv6" {
   zone_id = var.cloudflare_zone_id
-  name    = "pangolin-api"
+  name    = local.pangolin_api_subdomain
   content = hcloud_server.pangolin.ipv6_address
   type    = "AAAA"
   ttl     = 300
